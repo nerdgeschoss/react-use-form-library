e3ec9a1623d32c43005b8e72fe34af9c6e19306c
Jens Ravens <jens@nerdgeschoss.de>
2021-12-09 15:08:28 +0100

    simpler build with esbuild

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,6 @@
-import commonjs from 'rollup-plugin-commonjs';
-import resolve from 'rollup-plugin-node-resolve';
-import babel from 'rollup-plugin-babel';
+import esbuild from 'rollup-plugin-esbuild';
 import cleaner from 'rollup-plugin-cleaner';
 import pkg from './package.json';
-
-const extensions = ['.ts'];
 
 export default {
   input: './src/index.ts',
@@ -13,17 +9,12 @@ export default {
     cleaner({
       targets: ['./dist/'],
     }),
-    resolve({
-      extensions,
-    }),
-    commonjs(),
-    babel({
-      extensions,
-      include: ['src/**/*'],
-      runtimeHelpers: true,
+    esbuild({
+      target: 'es6',
     }),
   ],
-  output: [{
+  output: [
+    {
       file: pkg.main,
       format: 'cjs',
     },
